0ebd049005db9291f8da682e638930e364167934
Blackpride15 <asu23006@byui.edu>
2025-03-21 16:00:10 +0100

    commit

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,13 @@ document.getElementById('currentyear').textContent = new Date().getFullYear();
 
 // Get the last modified date of the document
 document.getElementById('lastModified').textContent = "Last Modification: " + document.lastModified;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerElement = document.getElementById('hamburger');
+    const navElement = document.querySelector('.rockstar');
+
+    hamburgerElement.addEventListener('click', () => {
+        navElement.classList.toggle('open');
+        hamburgerElement.classList.toggle('open');
+    });
+});
